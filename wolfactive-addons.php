d33c58185c9e77d710ee3695558378596ad614a9
Huy Nguyen <huyminhnguyenpham@gmail.com>
2025-01-06 09:00:31 +0700

    update: add logic load correct lanf

--- wolfactive-addons.php
+++ wolfactive-addons.php
@@ -72,11 +72,27 @@ add_shortcode( 'polylang_switcher', 'wolfactive_polylang_switcher' );
 
 function wolfactive_polylang_switcher() {
     if ( function_exists( 'pll_the_languages' ) ) {
-        return pll_the_languages( array(
-                'dropdown' => 1,
-                'echo' => 0,
-                'display_names_as' => 'slug'
-        ) );
+        $languages = pll_the_languages([
+            "dropdown" => 1,
+            "hide_current" => 0,
+            "show_flags" => 1,
+            "raw" => 1,
+            'display_names_as' => 'slug'
+        ]);
+        $output = '<select class="wolfactive-polylang-switcher">';
+        if ($languages) {
+            foreach ($languages as $lang) {
+                // $url returns the URL of the language, if the page exist in this language.
+                if ($lang['current_lang']) {
+                    $output .= '<option selected lang="'.$lang['locale'].'" value="' . $lang['url'] . '" class="wolfactive-polylang-switcher__item">' . $lang['name'] . '</option>';
+                }else{
+
+                    $output .= '<option lang="'.$lang['locale'].'" value="' . $lang['url'] . '" class="wolfactive-polylang-switcher__item">' . $lang['name'] . '</option>';
+                }
+            }
+        }
+        $output .= '</select>';
+        return $output;
     } else {
         return __( 'Polylang plugin is not active.', 'wolfactive-addons' );
     }
